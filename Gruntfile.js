3fbca241bdc5f1a88b67db4cb860afc5ef443849
infinityplusone <jonathan@infinityplusone.com>
2016-09-26 16:25:31 -0400

    more cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,48 +15,4 @@ module.exports = function(grunt) {
 
   grunt.loadTasks('tasks'); // custom tasks for necromancy
 
-  grunt.registerTask('rename', function() {
-    grunt.file.expand([
-      '**/bone**'
-    ]).forEach(function(f) {
-      if(grunt.file.isFile(f)) {
-        grunt.file.write(f.replace(/bone([^\/]+)$/, 'bone$1'), grunt.file.read(f));
-        grunt.file.delete(f);
-      }
-    });
-  });
-
-
-  grunt.registerTask('rebrand', function() {
-    grunt.file.expand([
-      '*.json',
-      'qore/**',
-      'tasks/**'
-    ]).forEach(function(f) {
-      if(grunt.file.isFile(f)) {
-        var contents = grunt.file.read(f);
-        if(f.indexOf('.json')>=0) {
-          contents = contents
-            .replace(/prodaq/gim, 'infinityplusone')
-            .replace('Nasdaq Product Design', 'Jonathan \"Yoni\" Knoll');
-        }
-        else {
-          contents = contents.replace(/prodaq/gim, 'brain');
-        }
-        contents = contents
-                    .replace(/([bB])loq/gm, '$1one')
-                    .replace(/manager/gm, 'skeleton')
-                    .replace(/Manager/gm, 'Skeleton')
-                    .replace(/mgr/gm, 'skel')
-                    .replace(/BoneManager/gm, 'Skeleton')
-                    .replace(/Qore/gm, 'Hippo')
-                    .replace(/qore(-data)?/gm, 'hippo');
-        
-        grunt.file.write(f.replace(/bone([^\/]+)$/, 'bone$1'), contents);
-        // grunt.file.delete(f);
-      }
-    });
-  });
-
-
 };
